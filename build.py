8cf5b59acabdf5422ba76701f190878a0116c69d
swainn <nswain@aquaveo.com>
2018-03-15 10:55:19 -0600

    Pass environment variables to dockers.

--- build.py
+++ build.py
@@ -4,11 +4,15 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     import os
     xmscore_version = os.environ.get('XMSCORE_VERSION', None)
-    travis_branch = os.environ.get('TRAVIS_BRANCH', None)
     print('XMSCORE_VERSION in build.py: {}'.format(xmscore_version))
-    print('TRAVIS_BRANCH in build.py: {}'.format(travis_branch))
+    # ConanPackageTools
+    # See: https://github.com/conan-io/conan-package-tools/blob/develop/README.md
     builder = ConanMultiPackager()
-    # To Add Configurations Explicitly:
-    # builder.add({"arch": "x86_64", "build_type": "Debug",  "compiler": "gcc", "compiler.version": "6", "compiler.libcxx": "libstdc++11"})  # noqa: F401
     builder.add_common_builds()
+    updated_builds = []
+    # Add environment variables
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        env_vars.update({'XMSCORE_VERSION': xmscore_version})
+        updated_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = updated_builds
     builder.run()
