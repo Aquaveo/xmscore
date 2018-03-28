905f24f02a55f1abd6e11d3cbca29109116d63ab
Gage Larsen <glarsen@aquaveo.com>
2018-03-28 15:56:15 -0600

    Tested and fixed setenv.bat script.

--- dev/setenv.bat
+++ dev/setenv.bat
@@ -1,9 +1,10 @@
+@echo off
 set XMSCORE_VERSION="1.0.7"
 set XMSCORE_RUN_TESTS="1"
-set CONAN_REFERENCE="xmscore/${XMSCORE_VERSION}"
+set CONAN_REFERENCE="xmscore/%XMSCORE_VERSION%"
 set CONAN_USERNAME="aquaveo"
 set CONAN_CHANNEL="stable"
 set CONAN_GCC_VERSIONS="5"
-# export CONAN_UPLOAD="1"
-set | findstr XMS
-set | findstr CONAN
+REM export CONAN_UPLOAD="1"
+set | findstr "XMS"
+set | findstr "CONAN"
