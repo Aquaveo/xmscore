8487e4160d08e863faa456720e9544ecbbcdc869
Gage Larsen <glarsen@aquaveo.com>
2018-03-29 12:22:33 -0600

    removed quotes and added visual version to environment

--- dev/setenv.bat
+++ dev/setenv.bat
@@ -1,10 +1,11 @@
 @echo off
-set XMS_VERSION="1.0.7"
-set XMS_RUN_TESTS="1"
-set CONAN_REFERENCE="xmscore/%XMS_VERSION%"
-set CONAN_USERNAME="aquaveo"
-set CONAN_CHANNEL="stable"
-set CONAN_GCC_VERSIONS="5"
+set XMS_VERSION=1.0.7
+set XMS_RUN_TESTS=1
+set CONAN_VISUAL_VERSIONS=12
+set CONAN_REFERENCE=xmscore/%XMS_VERSION%
+set CONAN_USERNAME=aquaveo
+set CONAN_CHANNEL=stable
+set CONAN_GCC_VERSIONS=5
 REM export CONAN_UPLOAD="<conanserver>"
 set | findstr "XMS"
 set | findstr "CONAN"
