f881183f3e1fb5d9ac32cd0836d2e85ccbdf183d
Gage Larsen <glarsen@aquaveo.com>
2018-04-12 14:54:47 -0600

    Updated setup.bat

---
+++ build/setup.bat
@@ -0,0 +1,25 @@
+@echo on
+mkdir x86\release
+cd x86\release
+conan install ..\..\.. -s compiler="Visual Studio" -s arch="x86" -s arch_build="x86" -s compiler.version="12" -s build_type="Release" -o xmscore:xms=True
+cmake -DBUILD_TESTING=1 -G "Visual Studio 12 2013" ..\..\..
+cd ..\..
+
+mkdir x86\debug
+cd x86\debug
+conan install ..\..\.. -s compiler="Visual Studio" -s arch="x86" -s arch_build="x86" -s compiler.version="12" -s build_type="Debug" -o xmscore:xms=True
+cmake -DBUILD_TESTING=1 -G "Visual Studio 12 2013" ..\..\..
+cd ..\..
+
+
+mkdir x64\release
+cd x64\release
+conan install ..\..\.. -s compiler="Visual Studio" -s arch="x86_64" -s arch_build="x86_64" -s compiler.version="12" -s build_type="Release" -o xmscore:xms=True
+cmake -DBUILD_TESTING=1 -G "Visual Studio 12 2013 Win64" ..\..\..
+cd ..\..
+
+mkdir x64\debug
+cd x64\debug
+conan install ..\..\.. -s compiler="Visual Studio" -s arch="x86_64" -s arch_build="x86_64" -s compiler.version="12" -s build_type="Debug" -o xmscore:xms=True
+cmake -DBUILD_TESTING=1 -G "Visual Studio 12 2013 Win64" ..\..\..
+cd ..\..
