14b0da801cffc0f736b502dfcb5fab5ade427163
nswain <nswain@aquaveo.com>
2018-04-30 10:36:21 -0600

    Add log_setup and reordered libraries in condabuildinfo.cmake

--- dev/setenv.sh
+++ dev/setenv.sh
@@ -1,10 +1,13 @@
-export XMS_VERSION="1.0.7"
+#!/usr/bin/env bash
+export XMS_VERSION="1.0.15"
 export XMS_RUN_TESTS="1"
 export CONAN_REFERENCE="xmscore/${XMS_VERSION}"
 export CONAN_USERNAME="aquaveo"
 export CONAN_CHANNEL="stable"
-export CONAN_GCC_VERSIONS="5"
-# export CONAN_UPLOAD="<conanserver>"
-# export CONAN_DOCKER_IMAGE="lasote/conangcc${CONAN_GCC_VERSIONS}"
+export CONAN_GCC_VERSIONS="6"
+export CONAN_ARCHS="x86_64"
+export CONAN_BUILD_TYPES="Release"
+#export CONAN_UPLOAD="<conanserver>"
+export CONAN_DOCKER_IMAGE="lasote/conangcc${CONAN_GCC_VERSIONS}"
 printenv | grep XMS
 printenv | grep CONAN
