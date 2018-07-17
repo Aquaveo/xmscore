b20b5caacc0d743a51aea81a8bd39135c4018389
Chandler Scott <cscott@aquaveo.com>
2018-07-17 08:11:21 -0600

    Make docker install lcov first

--- build.py
+++ build.py
@@ -1,18 +1,21 @@
+"""A Buildscript for XMSCore."""
 import os
 from conan.packager import ConanMultiPackager
-import time
 
 
 if __name__ == "__main__":
-    # ConanPackageTools
-    # See: https://github.com/conan-io/conan-package-tools/blob/develop/README.md
-    builder = ConanMultiPackager()
+    # ConanPackageTools, see:
+    # https://github.com/conan-io/conan-package-tools/blob/develop/README.md
+    docker_setup_commands = \
+        "sudo apt-get -qq update && sudo apt-get -qq install -y lcov"
+    builder = ConanMultiPackager(docker_entry_script=docker_setup_commands)
     builder.add_common_builds()
 
     # Add environment variables to build definitions
     xms_version = os.environ.get('XMS_VERSION', None)
 
-    for settings, options, env_vars, build_requires, reference in builder.items:
+    for settings, options, env_vars, build_requires, reference in \
+            builder.items:
         # General Options
         env_vars.update({
             'XMS_VERSION': xms_version,
@@ -24,39 +27,55 @@ if __name__ == "__main__":
                 'compiler.libcxx': 'libstdc++11'
             })
 
-
     pybind_updated_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
+    for settings, options, env_vars, build_requires, reference in \
+            builder.items:
         # pybind option
         if not settings['compiler'] == "apple-clang" \
-                and (not settings['compiler'] == "Visual Studio" \
+                and (not settings['compiler'] == "Visual Studio"
                      or int(settings['compiler.version']) > 12) \
                 and settings['arch'] == "x86_64":
             pybind_options = dict(options)
             pybind_options.update({'xmscore:pybind': True})
-            pybind_updated_builds.append([settings, pybind_options, env_vars, build_requires])
+            pybind_updated_builds.append(
+                [settings, pybind_options, env_vars, build_requires]
+            )
 
-        pybind_updated_builds.append([settings, options, env_vars, build_requires])
+        pybind_updated_builds.append(
+            [settings, options, env_vars, build_requires]
+        )
     builder.builds = pybind_updated_builds
 
     xms_updated_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
+    for settings, options, env_vars, build_requires, reference in \
+            builder.items:
         # xms option
-        if settings['compiler'] == 'Visual Studio' and 'MD' in settings['compiler.runtime']:
+        if settings['compiler'] == 'Visual Studio' \
+                and 'MD' in settings['compiler.runtime']:
             xms_options = dict(options)
             xms_options.update({'xmscore:xms': True})
-            xms_updated_builds.append([settings, xms_options, env_vars, build_requires])
-        xms_updated_builds.append([settings, options, env_vars, build_requires])
+            xms_updated_builds.append(
+                [settings, xms_options, env_vars, build_requires]
+            )
+        xms_updated_builds.append(
+            [settings, options, env_vars, build_requires]
+        )
     builder.builds = xms_updated_builds
 
     testing_updated_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
+    for settings, options, env_vars, build_requires, reference in \
+            builder.items:
         # xms option
-        if not options.get('xmscore:xms', False) and not options.get('xmscore:pybind', False):
+        if not options.get('xmscore:xms', False) \
+                and not options.get('xmscore:pybind', False):
             testing_options = dict(options)
             testing_options.update({'xmscore:testing': True})
-            testing_updated_builds.append([settings, testing_options, env_vars, build_requires])
-        testing_updated_builds.append([settings, options, env_vars, build_requires])
+            testing_updated_builds.append(
+                [settings, testing_options, env_vars, build_requires]
+            )
+        testing_updated_builds.append(
+            [settings, options, env_vars, build_requires]
+        )
     builder.builds = testing_updated_builds
 
     builder.run()
