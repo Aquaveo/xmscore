5b13eacabce0f63799f9d452688a4b78d5c6c4ba
Gage Larsen <glarsen@aquaveo.com>
2018-10-06 11:37:16 -0600

    don't deploy on non tags and don't allow warnings on python docs

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -114,7 +114,14 @@ ls $(dirname $SPHINX_CONF)
 # make a directory to hold the python documenation
 mkdir $(dirname $DOXYFILE)/pydocs
 # build the documentation
-sphinx-build -b html . $(dirname $DOXYFILE)/html/pydocs
+sphinx-build -W -b html . $(dirname $DOXYFILE)/html/pydocs
+
+
+# Back out if this is not a tag so we don't post
+if [ -z "${TRAVIS_TAG}" ]; then
+  echo "Build not tagged. No Documentation will be uploaded"
+  exit 0
+fi
 
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
