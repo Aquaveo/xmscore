a050b4d90d8d9c736fb4b40acf30b6f329820068
Gage Larsen <glarsen@aquaveo.com>
2018-10-08 12:11:57 -0600

    add class documentation

--- pydocs/source/conf.py
+++ pydocs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'pybuild', 'lib')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'build_py', 'lib')))
 
 
 # -- Project information -----------------------------------------------------
