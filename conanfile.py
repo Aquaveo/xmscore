90c46c49d6e35bc03e3254dc4ecb95b22b2c9e41
gagelarsen <glarsen@aquaveo.com>
2019-04-17 15:23:57 -0600

    run set

--- conanfile.py
+++ conanfile.py
@@ -95,6 +95,7 @@ class XmscoreConan(ConanFile):
                 # Create and upload wheel to PyPi if release and windows
                 is_release = self.env.get("RELEASE_PYTHON", 'False')
                 print("*****IS RELEASE=={}*****".format(is_release))
+                self.run('set')
                 if self.settings.os == "Windows" and is_release == 'True':
                     self.run('python setup.py bdist_wheel --plat-name=win_amd64 --dist-dir {}'.format(
                         os.path.join(self.build_folder, "dist")), cwd=os.path.join(self.package_folder, "_package"))
