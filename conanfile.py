cf26790541283b581a1dff3efe76a0176417034a
gagelarsen <glarsen@aquaveo.com>
2019-05-10 14:45:51 -0600

    add plat names for other products

--- conanfile.py
+++ conanfile.py
@@ -97,7 +97,9 @@ class XmscoreConan(ConanFile):
                 if is_release == 'True' and ((self.settings.os == "Macos" or self.settings.os == "Linux")
                                              or (self.settings.os == "Windows" and
                                              str(self.settings.compiler.runtime) == "MD")):
-                    self.run('python setup.py bdist_wheel --plat-name=win_amd64 --dist-dir {}'.format(
+                    plat_names = {'Windows': 'win_amd64', 'Linux': 'linux_x86_64', "Macos": 'macosx'}
+                    self.run('python setup.py bdist_wheel --plat-name={} --dist-dir {}'.format(
+                        plat_names[str(self.settings.os)],
                         os.path.join(self.build_folder, "dist")), cwd=os.path.join(self.package_folder, "_package"))
                     self.run('twine upload dist/*', cwd=".")
 
