b316ec5eebcbbd8009c4e7430b05887dbbac83b0
gagelarsen <glarsen@aquaveo.com>
2019-05-13 13:23:30 -0600

    versions not version

--- .travis/run.sh
+++ .travis/run.sh
@@ -8,8 +8,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
     pyenv activate conan
-    export PATH=$(pyenv root)/version/3.6.8/bin:$PATH
-    ls $(pyenv root)
+    export PATH=$(pyenv root)/versions/3.6.8/bin:$PATH
+    ls $(pyenv root)/versions/3.6.8/bin
 else
     sudo docker login -u ${AQUAVEO_DOCKER_USERNAME_SECRET} -p ${AQUAVEO_DOCKER_USERNAME_PASSWORD} docker.aquaveo.com
 fi
