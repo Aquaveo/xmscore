58e71ef4ea3b9db95c0cb6cd2dc7ef1e977a3c69
gagelarsen <glarsen@aquaveo.com>
2019-05-13 09:42:54 -0600

    try dev

--- .travis/run.sh
+++ .travis/run.sh
@@ -7,7 +7,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
-#    pyenv activate conan
+    pyenv activate conan
 else
     sudo docker login -u ${AQUAVEO_DOCKER_USERNAME_SECRET} -p ${AQUAVEO_DOCKER_USERNAME_PASSWORD} docker.aquaveo.com
 fi
