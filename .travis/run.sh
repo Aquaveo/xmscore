337102d7a886812f55016b303608cc8559c0a4fa
gagelarsen <glarsen@aquaveo.com>
2019-05-13 11:47:20 -0600

    forgot the = sign

--- .travis/run.sh
+++ .travis/run.sh
@@ -8,7 +8,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
     pyenv activate conan
-    export $(pyenv root)/version/3.6.8/bin:$PATH
+    export PATH=$(pyenv root)/version/3.6.8/bin:$PATH
 else
     sudo docker login -u ${AQUAVEO_DOCKER_USERNAME_SECRET} -p ${AQUAVEO_DOCKER_USERNAME_PASSWORD} docker.aquaveo.com
 fi
