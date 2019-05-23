ef6d22aafdcfe8cbc4c2837680660f34c7c6ab94
Bill Dolinar <bdolinar@aquaveo.com>
2019-05-23 15:48:12 -0600

    Added ability to DaStreamReader to see what current line starts with without moving stream position from begining of line. Added ability to read and write binary VecDbl.

--- xmscore/dataio/daStreamIo.h
+++ xmscore/dataio/daStreamIo.h
@@ -39,14 +39,17 @@ public:
   ~DaStreamReader();
 
   bool IsBinary() const;
+
   bool ReadNamedLine(const char* a_name);
   bool ReadLine(std::string& a_line);
+  bool ReadStringLine(const char* a_name, std::string& a_val);
   bool ReadIntLine(const char* a_name, int& a_val);
   bool ReadDoubleLine(const char* a_name, double& a_val);
-  bool ReadStringLine(const char* a_name, std::string& a_val);
+
   bool ReadVecInt(const char* a_name, VecInt& a_vec);
   bool ReadVecDbl(const char* a_name, VecDbl& a_vec);
   bool ReadVecPt3d(const char* a_name, VecPt3d& a_vec);
+
   bool Read2StringLine(const char* a_name, std::string& a_val1, std::string& a_val2);
   bool Read3StringLine(const char* a_name,
                        std::string& a_val1,
@@ -64,6 +67,8 @@ public:
 
   bool ReadBinaryBytes(char* a_dest, long long a_destLength);
 
+  bool LineBeginsWith(const char* a_text);
+
 private:
   XM_DISALLOW_COPY_AND_ASSIGN(DaStreamReader)
   class Impl;
@@ -78,25 +83,25 @@ public:
   ~DaStreamWriter();
 
   bool IsBinary() const;
+  
+  void WriteLine(const std::string& a_line);
+  void WriteStringLine(const char* a_name, const std::string& a_val);
+  void WriteIntLine(const char* a_name, int a_val);
+  void WriteDoubleLine(const char* a_name, double a_val);
+
   void WriteVecInt(const char* a_name, const VecInt& a_vec);
   void WriteVecDbl(const char* a_name, const VecDbl& a_vec);
   void WriteVecPt3d(const char* a_name, const VecPt3d& a_points);
 
-  void WriteIntLine(const char* a_name, int a_val);
-
-  void WriteDoubleLine(const char* a_name, double a_val);
-  void Write3DoubleLine(const char* a_name,
-                        const double& a_val1,
-                        const double& a_val2,
-                        const double& a_val3);
-
-  void WriteLine(const std::string& a_line);
-  void WriteStringLine(const char* a_name, const std::string& a_val);
   void Write2StringLine(const char* a_name, const std::string& a_val1, const std::string& a_val2);
   void Write3StringLine(const char* a_name,
                         const std::string& a_val1,
                         const std::string& a_val2,
                         const std::string& a_val3);
+  void Write3DoubleLine(const char* a_name,
+                        const double& a_val1,
+                        const double& a_val2,
+                        const double& a_val3);
 
   void WriteString(const char* a_string);
   void AppendInt(int a_val);
