2291c089bad5433fbc8ef3be7010a0d23d48c1d4
Michael Kennard <mkennard@aquaveo.com>
2020-08-07 16:22:02 -0600

    Added filesystem.make_filename_unique and a test

--- _package/tests/unit_tests/filesystem_pyt.py
+++ _package/tests/unit_tests/filesystem_pyt.py
@@ -54,3 +54,28 @@ class FilesystemTests(unittest.TestCase):
         self.assertEqual(os.path.normpath(os.path.dirname(f)), os.path.normpath(d))
 
         shutil.rmtree(d)
+
+    def test_make_filename_unique(self):
+        """See name."""
+        # Create a temporary directory to do our test in
+        with tempfile.TemporaryDirectory() as directory:
+            filepath = os.path.join(directory, 'make_unique.html')
+
+            # Verify that make_filename_unique just returns the filename if it doesn't exist
+            self.assertEqual(False, os.path.exists(filepath))
+            filepath_unique = filesystem.make_filename_unique(filepath)
+            self.assertEqual(filepath, filepath_unique)
+
+            # Verify that make_filename_unique returns a unique filename if it does exist
+            with open(filepath, 'w'):
+                self.assertEqual(True, os.path.exists(filepath))
+                filepath_unique = filesystem.make_filename_unique(filepath)
+                self.assertEqual(os.path.dirname(filepath), os.path.dirname(filepath_unique))
+                self.assertEqual('make_unique(2).html', os.path.basename(filepath_unique))
+
+                # Verify that it will loop until it finds a unique name
+                with open(filepath_unique, 'w'):
+                    self.assertEqual(True, os.path.exists(filepath_unique))
+                    filepath_unique2 = filesystem.make_filename_unique(filepath)
+                    self.assertEqual(os.path.dirname(filepath), os.path.dirname(filepath_unique2))
+                    self.assertEqual('make_unique(3).html', os.path.basename(filepath_unique2))
