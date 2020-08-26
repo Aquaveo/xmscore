ed334ce438071152790d73685593822ac3346e8a
Bill Dolinar <bdolinar@aquaveo.com>
2020-08-26 11:49:02 -0600

    Changed versions of boost to 1.60.0 for xms builds, 1.66.0 for other windows builds, and 1.73.0 for linux and mac builds.

--- conanfile.py
+++ conanfile.py
@@ -202,8 +202,10 @@ class XmscoreConan(ConanFile):
         if self.options.xms and self.settings.compiler.version == "12":
             self.requires("boost/1.60.0@aquaveo/stable")
             self.requires("zlib/1.2.11@conan/stable")
-        else:
+        elif self.settings.compiler == 'Visual Studio':
             self.requires("boost/1.66.0@conan/stable")
+        else:
+            self.requires("boost/1.73.0")
         # Pybind if not Visual studio 2013 or clang
         if not self.settings.compiler == "clang" \
                 and not (self.settings.compiler == 'Visual Studio' and self.settings.compiler.version == "12") \
