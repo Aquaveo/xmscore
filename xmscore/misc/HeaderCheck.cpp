afec5cd93c5231e71f4c607c2bd54cb4a36cd889
aclark <aclark@aquaveo.com>
2020-11-06 09:37:13 -0700

    Try to generate errors.

---
+++ xmscore/misc/HeaderCheck.cpp
@@ -0,0 +1,62 @@
+//------------------------------------------------------------------------------
+/// \file
+/// \ingroup misc
+/// \note This is just here to include all headers and make sure they compile.
+/// \copyright (C) Copyright Aquaveo 2018. Distributed under FreeBSD License
+/// (See accompanying file LICENSE or https://aqaveo.com/bsd/license.txt)
+//------------------------------------------------------------------------------
+
+//----- Included files ---------------------------------------------------------
+
+// 1. Precompiled header
+
+// 2. My own header
+
+// 3. Standard library headers
+
+// 4. External library headers
+
+// 5. Shared code headers
+
+// 6. Non-shared code headers
+#include <xmscore/dataio/daStreamIo.h>
+#include <xmscore/math/math.h>
+#include <xmscore/misc/base_macros.h>
+#include <xmscore/misc/boost_defines.h>
+#include <xmscore/misc/carray.h>
+#include <xmscore/misc/DynBitset.h>
+#include <xmscore/misc/environment.h>
+#include <xmscore/misc/Observer.h>
+#include <xmscore/misc/Progress.h>
+#include <xmscore/misc/Singleton.h>
+#include <xmscore/misc/StringUtil.h>
+#include <xmscore/misc/XmConst.h>
+#include <xmscore/misc/XmError.h>
+#include <xmscore/misc/XmLog.h>
+#include <xmscore/misc/xmstype.h>
+#include <xmscore/points/functors.h>
+#include <xmscore/points/pt.h>
+#include <xmscore/points/ptsfwd.h>
+#include <xmscore/points/ptsfwd.h>
+#include <xmscore/stl/deque.h>
+#include <xmscore/stl/hash.h>
+#include <xmscore/stl/list.h>
+#include <xmscore/stl/map.h>
+#include <xmscore/stl/set.h>
+#include <xmscore/stl/utility.h>
+#include <xmscore/stl/vector.h>
+#include <xmscore/testing/TestTools.h>
+
+//----- Forward declarations ---------------------------------------------------
+
+//----- External globals -------------------------------------------------------
+
+//----- Namespace declaration --------------------------------------------------
+
+//----- Constants / Enumerations -----------------------------------------------
+
+//----- Classes / Structs ------------------------------------------------------
+
+//----- Internal functions -----------------------------------------------------
+
+//----- Class / Function definitions -------------------------------------------
