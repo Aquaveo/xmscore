4c0a6f32d0fb91e4b6e3e18a235fa854fcad89b5
Thomas Christiansen <tchristiansen@aquaveo.local>
2022-03-02 11:21:32 -0700

    Mark observer_py.cpp.

--- xmscore/python/misc/observer_py.cpp
+++ xmscore/python/misc/observer_py.cpp
@@ -19,7 +19,7 @@ namespace py = pybind11;
 PYBIND11_DECLARE_HOLDER_TYPE(T, boost::shared_ptr<T>);
 
 void initObserver(py::module &m) {
-  py::class_<xms::PublicObserver, PyObserver, boost::shared_ptr<xms::PublicObserver>> observer(m, "Observer");
+  py::class_<xms::PublicObserver, PyObserver, boost::shared_ptr<xms::PublicObserver>> observer(m, N_("Observer"));
 
   // Initializer
   observer.def(py::init());
@@ -27,58 +27,58 @@ void initObserver(py::module &m) {
   // ---------------------------------------------------------------------------
   // function: progress_status
   // ---------------------------------------------------------------------------
-  observer.def("progress_status", &xms::PublicObserver::ProgressStatus, 
-               py::arg("percent_complete"));
+  observer.def(N_("progress_status"), &xms::PublicObserver::ProgressStatus, 
+               py::arg(N_("percent_complete")));
 
   // ---------------------------------------------------------------------------
   // function: begin_operation_string
   // ---------------------------------------------------------------------------
-  observer.def("begin_operation_string", &xms::PublicObserver::BeginOperationString,
-               py::arg("operation"));
+  observer.def(N_("begin_operation_string", &xms::PublicObserver::BeginOperationString,
+               py::arg(N_("operation")));
 
   // ---------------------------------------------------------------------------
   // function: end_operation
   // ---------------------------------------------------------------------------
-  observer.def("end_operation", &xms::PublicObserver::EndOperation);
+  observer.def(N_("end_operation"), &xms::PublicObserver::EndOperation);
 
   // ---------------------------------------------------------------------------
   // function: update_message
   // ---------------------------------------------------------------------------
-  observer.def("update_message", &xms::PublicObserver::UpdateMessage,
-               py::arg("message"));
+  observer.def(N_("update_message"), &xms::PublicObserver::UpdateMessage,
+               py::arg(N_("message")));
 
   // ---------------------------------------------------------------------------
   // function: on_progress_status
   // ---------------------------------------------------------------------------
-  observer.def("on_progress_status", &xms::PublicObserver::on_progress_status,
-               py::arg("percent_complete"));
+  observer.def(N_("on_progress_status"), &xms::PublicObserver::on_progress_status,
+               py::arg(N_("percent_complete")));
 
   // ---------------------------------------------------------------------------
   // function: on_begin_operation_string
   // ---------------------------------------------------------------------------
-  observer.def("on_begin_operation_string", &xms::PublicObserver::on_begin_operation_string,
-               py::arg("operation"));
+  observer.def(N_("on_begin_operation_string"), &xms::PublicObserver::on_begin_operation_string,
+               py::arg(N_("operation"));
 
   // ---------------------------------------------------------------------------
   // function: on_end_operation
   // ---------------------------------------------------------------------------
-  observer.def("on_end_operation", &xms::PublicObserver::on_end_operation);
+  observer.def(N_("on_end_operation"), &xms::PublicObserver::on_end_operation);
 
   // ---------------------------------------------------------------------------
   // function: on_update_message
   // ---------------------------------------------------------------------------
-  observer.def("on_update_message", &xms::PublicObserver::on_update_message,
-               py::arg("message"));
+  observer.def(N_("on_update_message"), &xms::PublicObserver::on_update_message,
+               py::arg(N_("message")));
 
   // ---------------------------------------------------------------------------
   // function: time_remaining_in_seconds
   // ---------------------------------------------------------------------------
-  observer.def("time_remaining_in_seconds", &xms::PublicObserver::time_remaining_in_seconds,
-               py::arg("seconds_remaining"));
+  observer.def(N_("time_remaining_in_seconds"), &xms::PublicObserver::time_remaining_in_seconds,
+               py::arg(N_("seconds_remaining")));
 
   // ---------------------------------------------------------------------------
   // function: time_elapsed_in_seconds
   // ---------------------------------------------------------------------------
-  observer.def("time_elapsed_in_seconds", &xms::PublicObserver::time_elapsed_in_seconds,
-               py::arg("elapsed_seconds"));
+  observer.def(N_("time_elapsed_in_seconds"), &xms::PublicObserver::time_elapsed_in_seconds,
+               py::arg(N_("elapsed_seconds")));
 }
