8f5dc9d2e39e7aab1fe4a11508e7e95a45c25244
Thomas Christiansen <tchristiansen@aquaveo.local>
2022-03-04 12:48:34 -0700

    Add a translate wrapper.

--- _package/xms/core/locale/__init__.py
+++ _package/xms/core/locale/__init__.py
@@ -5,3 +5,6 @@ from .._xmscore.locale import add_message_path, add_text_domain, translate
 
 def n_(message):
     return message
+
+def translate_for_domain(domain):
+    return lambda message: translate(message, domain)
