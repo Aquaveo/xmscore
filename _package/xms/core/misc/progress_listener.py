3ec3eb9eef9cc005f282a39dd55fde4c8f30f2f0
Alan <37482427+alemon-aquaveo@users.noreply.github.com>
2022-12-01 16:40:19 -0700

    -update progress listener

--- _package/xms/core/misc/progress_listener.py
+++ _package/xms/core/misc/progress_listener.py
@@ -4,6 +4,20 @@ The Observer class for the xms.core library.
 from .._xmscore.misc import ProgressListener as Prog
 
 
+def set_listener_callback(call_back):
+    """Sets a call back method that is called by the progress listener class.
+
+    Args:
+        call_back (callable): takes a tuple (string, int, string) (msg_type, stack_index, message)
+
+    Returns:
+         (ProgressListener): this class with the call back set
+    """
+    p = ProgressListener()
+    p.call_back = call_back
+    return p
+
+
 class ProgressListener(Prog):
     """
     Mock Observer class for testing.
@@ -22,9 +36,8 @@ class ProgressListener(Prog):
             stack_index (int): stack index for the progress
             percent_complete (float):  The percent complete
         """
-        self.on_progress_status(stack_index, percent_complete)
         msg = f'Percent complete {int(100 * percent_complete)}'
-        self.message_received('progress_status', stack_index, msg)
+        self.call_back(('progress_status', stack_index, msg))
 
     def on_begin_operation_string(self, operation):
         """
@@ -37,15 +50,14 @@ class ProgressListener(Prog):
             (int): stack index for the operation
         """
         stack_index = self.on_begin_operation_string(operation)
-        self.message_received('begin_operation', stack_index, operation)
+        self.call_back(('begin_operation', stack_index, operation))
         return stack_index
 
     def on_end_operation(self, stack_index):
         """
         The end operation event.
         """
-        self.on_end_operation(stack_index)
-        self.message_received('end_operation', stack_index, '')
+        self.call_back(('end_operation', stack_index, ''))
 
     def on_update_message(self, stack_index, message):
         """
@@ -55,15 +67,12 @@ class ProgressListener(Prog):
             stack_index (int): stack index for the progress
             message: The new message
         """
-        self.on_update_message(stack_index, message)
-        self.message_received('update_message', -1, message)
+        self.call_back(('update_message', -1, message))
 
-    def message_received(self, msg_type, stack_index, message):
+    def call_back(self, msg_tuple):
         """Convenience function.
 
         Args:
-            msg_type (str): type of message
-            stack_index (int): stack index for the progress
-            message: The new message
+            msg_tuple (tuple(str, int, str)): msg_type, stack_index, message
         """
         pass
