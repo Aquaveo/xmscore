58c3d53e6b2a78b49af1746ddfd4a49c986e65e8
Michael Kennard <mkennard@aquaveo.com>
2023-12-06 11:29:08 -0700

    Added support for pathlib in filesystem, and added type hints and tests
    (cherry picked from commit 1c821c084e9f446896e41eaeb31ef9565e53c7d0)

--- _package/xms/core/filesystem/filesystem.py
+++ _package/xms/core/filesystem/filesystem.py
@@ -17,14 +17,13 @@ __copyright__ = "(C) Copyright Aquaveo 2019"
 __license__ = "All rights reserved"
 
 
-def clear_folder(folder):
+def clear_folder(folder: str | Path) -> None:
     """Deletes everything in folder.
 
     From https://stackoverflow.com/questions/185936
 
     Args:
-        folder (str): The folder to clear
-
+        folder: The folder to clear.
     """
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
@@ -37,12 +36,11 @@ def clear_folder(folder):
             print(e)
 
 
-def make_or_clear_dir(output_dir):
+def make_or_clear_dir(output_dir: str | Path) -> None:
     """Makes or clears the output directory.
 
     Args:
-        output_dir (str): The folder to make or clear
-
+        output_dir: The folder to make or clear.
     """
     if os.path.isdir(output_dir):
         clear_folder(output_dir)
@@ -50,27 +48,25 @@ def make_or_clear_dir(output_dir):
         os.makedirs(output_dir, exist_ok=True)
 
 
-def paths_are_equal(path1, path2):
+def paths_are_equal(path1: str | Path, path2: str | Path) -> bool:
     """Returns true if the two file paths are equal.
 
     Args:
-        path1 (str): The first path.
-        path2 (str): The second path.
+        path1: The first path.
+        path2: The second path.
 
     Returns:
-        (bool) True if they are equal
-
+        True if they are equal.
     """
     return os.path.normpath(path1) == os.path.normpath(path2)
 
 
-def copyfile(src, dest):
+def copyfile(src: str | Path, dest: str | Path) -> None:
     """Copy a file on disk, suppressing errors if source and destination are the same file.
 
     Args:
-        src (str): Source file path.
-        dest (str): Destination file path.
-
+        src: Source file path.
+        dest: Destination file path.
     """
     try:
         shutil.copyfile(src, dest)
@@ -78,12 +74,11 @@ def copyfile(src, dest):
         pass
 
 
-def removefile(filename):
+def removefile(filename: str | Path) -> None:
     """Delete a file from disk, ignoring any errors.
 
     Args:
-        filename (str): Path of file to be removed.
-
+        filename: Path of file to be removed.
     """
     try:
         os.remove(filename)
@@ -91,16 +86,15 @@ def removefile(filename):
         pass
 
 
-def resolve_relative_path(absolute_path, relative_path):
+def resolve_relative_path(absolute_path: str | Path, relative_path: str | Path) -> str:
     """Given a path and a path that is relative to it, return the full path.
 
     Args:
-        absolute_path (str): An absolute path.
-        relative_path (str): A path relative to the absolute path.
+        absolute_path: An absolute path.
+        relative_path: A path relative to the absolute path.
 
     Returns:
         The full path to the item indicated by relative_path.
-
     """
     # If they give us a filename, try to get just the path
     normpath = ''
@@ -109,25 +103,24 @@ def resolve_relative_path(absolute_path, relative_path):
             return os.path.normpath(relative_path)
         if Path(absolute_path).is_file():
             absolute_path = os.path.dirname(absolute_path)
-        resolved_path = os.path.join(absolute_path, relative_path.strip("'"))
+        resolved_path = os.path.join(absolute_path, str(relative_path).strip("'"))
         normpath = os.path.normpath(resolved_path)
     except Exception:
         pass
     return normpath
 
 
-def does_file_exist(file, proj_dir):
+def does_file_exist(file: str | Path, proj_dir: str | Path) -> bool:
     """Determine if a file in our persistent data still exist.
 
     If file is not absolute, will check if relative from the project directory exists.
 
     Args:
-       file (str): Relative or absolute file path to check the existence of
-       proj_dir (str): Project path to resolve relative paths to
+       file: Relative or absolute file path to check the existence of
+       proj_dir: Project path to resolve relative paths to
 
     Returns:
-        (bool): True if the file exists
-
+        True if the file exists.
     """
     try:
         if not os.path.isabs(file):  # Convert relative to absolute
@@ -137,7 +130,7 @@ def does_file_exist(file, proj_dir):
         return False
 
 
-def compute_relative_path(path, file):
+def compute_relative_path(path: str | Path, file: str | Path) -> str:
     r"""Computes and returns the path of file relative to path.
 
     Also changes all '\' to '/' so tests work on Windows and Linux.
@@ -145,11 +138,11 @@ def compute_relative_path(path, file):
     If the paths are on different drives, the original "file" path is returned (but with '\' to '/')
 
     Args:
-        path (str): A full path included in 'file'.
-        file (str): A full path to a file.
+        path: A full path included in 'file'.
+        file: A full path to a file.
 
     Returns:
-        str: A relative path.
+        A relative path.
     """
     try:
         return os.path.relpath(file, path).replace('\\', '/')
@@ -157,48 +150,46 @@ def compute_relative_path(path, file):
         return file.replace('\\', '/')
 
 
-def is_somewhere_below_system_temp(filename):
+def is_somewhere_below_system_temp(filename: str | Path) -> bool:
     """Returns true if a component is locked, given its main_file.
 
     Args:
-        filename (str): The component main_file name.
+        filename: The file.
 
     Returns:
         See description.
-
     """
     temp_dir = Path(tempfile.gettempdir())
     filepath = Path(filename)
     return temp_dir in filepath.parents
 
 
-def file_prefix(filepath):
+def file_prefix(filepath: str | Path) -> str:
     """Returns the first part of the filename; no extension, no path.
 
     Args:
-        filepath (str): The path and filename.
+        filepath: The path and filename.
 
     Returns:
         See description.
-
     """
     basename = os.path.basename(filepath)
     return os.path.splitext(basename)[0]
 
 
-def temp_filename(dir='', suffix=''):
+def temp_filename(dir_: str | Path = None, suffix: str = '') -> str:
     """Returns a temporary filename, in the XMS temp directory by default.
 
     Args:
-        dir: If provided, filename will be in the directory. Otherwise it will be in the
+        dir_: If provided, filename will be in the directory. Otherwise it will be in the
              system temp directory.
         suffix: The suffix to use for the file. You must include a '.' if you want it to be an extension.
 
     Returns:
         See description.
     """
-    if dir:  # If we call the next line with dir == '', it seems to use the working directory.
-        file = tempfile.NamedTemporaryFile(mode='wt', suffix=suffix, dir=dir, delete=True)
+    if dir_:  # If we call the next line with dir_ == '', it seems to use the working directory.
+        file = tempfile.NamedTemporaryFile(mode='wt', suffix=suffix, dir=dir_, delete=True)
     else:
         xms_temp = os.environ.get('XMS_PYTHON_APP_TEMP_DIRECTORY', 'unknown')
         if xms_temp != 'unknown':
@@ -211,19 +202,19 @@ def temp_filename(dir='', suffix=''):
     return filename
 
 
-def make_filename_unique(filepath):
+def make_filename_unique(filepath: str | Path) -> str | None:
     """Appends a suffix to the file if needed to make it unique.
 
     Example: file.txt becomes file(2).txt
 
     Args:
-        filepath (str): Filepath to consider.
+        filepath: Filepath to consider.
 
     Returns:
-        The unique filename.
+        The unique filename or None if it fails.
     """
     if not os.path.exists(filepath):
-        return filepath
+        return str(filepath)
 
     path, name = os.path.split(filepath)
     name, extension = os.path.splitext(name)
