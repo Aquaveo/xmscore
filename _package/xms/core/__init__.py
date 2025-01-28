8647a0cde7e161b543dd3d9d229f27d69fa99586
Bill Dolinar <bill_dolinar@yahoo.com>
2025-01-28 12:53:21 -0700

    Changed to pyproject.toml file.
    Changed the Python package to use a pyproject.toml file. Removed build.py, CMakeLists.txt, and conanfile.py. They can be generated with xmsconan_gen.

--- _package/xms/core/__init__.py
+++ _package/xms/core/__init__.py
@@ -4,5 +4,3 @@ The __init__.py for the core module of the xms.core library.
 from . import filesystem  # NOQA: F401
 from . import misc  # NOQA: F401
 from . import time  # NOQA: F401
-
-__version__ = '6.2.5'
